c24438abdf0ddb844eb3e59280214fe769760c07
Your Name <you@example.com>
2024-06-10 14:25:04 +0100

    application is working properly and is able to install a pwa version of JATE

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,9 +1,12 @@
 const butInstall = document.getElementById('buttonInstall');
 
+// Logic for installing the PWA
 window.addEventListener('beforeinstallprompt', (event) => {
 
+    // Store the triggered events
     window.deferredPrompt = event;
 
+    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
   });
 
@@ -15,14 +18,16 @@ butInstall.addEventListener('click', async () => {
    return;
   }
 
+  // Show prompt
   promptEvent.prompt();
   
+  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-
+  // Clear prompt
   window.deferredPrompt = null;
 }); 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +15,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // This plugin creates an HTML file to serve your webpack bundles and automatically injects all your bundles to this file.
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+
+      // InjectManifest plugin for configuring the service worker with workbox
+      new InjectManifest({
+        swSrc: './src-sw.js', // Updated source of your service worker file
+        swDest: 'src-sw.js', // Updated destination filename in output directory
+        exclude: [/\.map$/, /asset-manifest\.json$/] // Exclude maps and manifest to avoid caching them
+      }),
+
+      // This plugin generates a 'manifest.json' for Progressive Web App
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'A simple text editor as a PWA',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '.',
+        publicPath: '.',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes for different resolutions
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], // Loaders for processing CSS
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader', // Babel loader to transpile modern JavaScript to backward compatible versions
+            options: {
+              presets: ['@babel/preset-env'], // Preset for compiling ES2015+ syntax
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'], // Plugins for Babel to support modern JavaScript features
+            },
+          },
+        },
       ],
     },
   };
